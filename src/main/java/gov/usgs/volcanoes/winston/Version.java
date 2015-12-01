898855605497c7bf8f9cdde2ac0b511173f0e93f
Tom Parker <tparker@usgs.gov>
2015-11-30 20:38:20 -0900

    Add log4j props and ignore Version.java

--- src/main/java/gov/usgs/volcanoes/winston/Version.java
+++ src/main/java/gov/usgs/volcanoes/winston/Version.java
@@ -2,9 +2,9 @@ package gov.usgs.volcanoes.winston;
 
 public final class Version {
 
-  public static final String BUILD_TIME = "2015-11-30T22:12:31Z";
+    public static final String BUILD_TIME="2015-12-01T03:12:10Z";
 
-  public static final String POM_VERSION = "1.3.0-SNAPSHOT";
+    public static final String POM_VERSION="1.3.0-SNAPSHOT";
 
-  public static final String VERSION_STRING = "Version: " + POM_VERSION + " Built: " + BUILD_TIME;
+	public static final String VERSION_STRING = "Version: " + POM_VERSION + " Built: " + BUILD_TIME;
 }
