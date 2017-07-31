1e62097b0f65e4c6bb5d63ec7300707ea0a6da43
tparker <tparker@usgs.gov>
2017-07-30 16:58:31 -0800

    cleanup

--- src/main/java/gov/usgs/volcanoes/winston/server/wws/cmd/EwDataRequest.java
+++ src/main/java/gov/usgs/volcanoes/winston/server/wws/cmd/EwDataRequest.java
@@ -6,14 +6,12 @@
 
 package gov.usgs.volcanoes.winston.server.wws.cmd;
 
-import gov.usgs.volcanoes.core.data.Scnl;
 import gov.usgs.volcanoes.core.util.UtilException;
 import gov.usgs.volcanoes.winston.db.Channels;
 import gov.usgs.volcanoes.winston.db.Data;
 import gov.usgs.volcanoes.winston.db.WinstonDatabase;
 import gov.usgs.volcanoes.winston.server.wws.WinstonConsumer;
 import gov.usgs.volcanoes.winston.server.wws.WwsBaseCommand;
-import gov.usgs.volcanoes.winston.server.wws.WwsCommandString;
 
 /**
  * Abstract request for data using an Earthworm WSV-style command
@@ -22,40 +20,20 @@ import gov.usgs.volcanoes.winston.server.wws.WwsCommandString;
  *
  */
 public abstract class EwDataRequest extends WwsBaseCommand {
-//  protected Scnl scnl;
-
-//  protected String getChan(WwsCommandString cmd, String delimiter) {
-//    scnl = cmd.scnl;
-//    final String s = cmd.getS();
-//    final String c = cmd.getC();
-//    final String n = cmd.getN();
-//
-//    String scnl = s + delimiter + c + delimiter + n;
-//
-//    if (isScnl) {
-//      final String l = cmd.getL();
-//      if (!"--".equals(l)) {
-//        scnl += delimiter + l;
-//      }
-//    }
-//
-//    return scnl;
-//  }
-
   protected Integer getChanId(final String code) throws UtilException {
     final Integer chanId;
-  try {
-    chanId = databasePool.doCommand(new WinstonConsumer<Integer>() {
-      public Integer execute(WinstonDatabase winston) throws UtilException {
-        return new Channels(winston).getChannelID(code);
-      }
-    });
-  } catch (Exception e) {
-    throw new UtilException("Unable to get chanId");
+    try {
+      chanId = databasePool.doCommand(new WinstonConsumer<Integer>() {
+        public Integer execute(WinstonDatabase winston) throws UtilException {
+          return new Channels(winston).getChannelID(code);
+        }
+      });
+    } catch (Exception e) {
+      throw new UtilException("Unable to get chanId");
+    }
+    return chanId;
   }
-  return chanId;
-  }
-  
+
   protected double[] getTimeSpan(final Integer chanId) throws UtilException {
     double[] timeSpan;
     try {
@@ -69,5 +47,4 @@ public abstract class EwDataRequest extends WwsBaseCommand {
     }
     return timeSpan;
   }
-
 }
