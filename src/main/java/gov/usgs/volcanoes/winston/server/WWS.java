9e4a7260a45962b3a68881d8c4469144f8ffaaf6
Tom Parker <tparker@usgs.gov>
2017-09-15 11:44:17 -0800

    cleanup.

--- src/main/java/gov/usgs/volcanoes/winston/server/WWS.java
+++ src/main/java/gov/usgs/volcanoes/winston/server/WWS.java
@@ -122,11 +122,8 @@ public class WWS {
     sb.append(Version.VERSION_STRING + "\n");
     sb.append("Keys:\n");
     sb.append("      0-3: logging level\n");
-    // sb.append(" d: drop idle connections\n");
     sb.append("        q: quit\n");
     sb.append("        ?: display keys\n");
-    // sb.append(" m: print running commands\n");
-    // sb.append(" t<index>: toggle tracing of a connection\n");
     sb.append("        c: print connections sorted by bytes transmited\n");
     sb.append("       cA: print connections sorted by address\n");
     sb.append("       cC: print connections sorted by connect time\n");
